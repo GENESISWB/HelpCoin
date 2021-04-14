a6dbbeab06c2ddbf1258e964de998ed3ee95f6ef
Liam <78091151+liamkelsey@users.noreply.github.com>
2021-04-14 15:56:21 +0100

    Update HelpCoin.sol

--- contracts/HelpCoin.sol
+++ contracts/HelpCoin.sol
@@ -6,34 +6,8 @@ import "./_external/ERC20Detailed.sol";
 
 import "./lib/SafeMathInt.sol";
 
-/**
- * @title HelpCoin ERC20 token
- * @dev This is part of an implementation of the HelpCoin Ideal Money protocol.
- *      HelpCoin is a normal ERC20 token, but its supply can be adjusted by splitting and
- *      combining tokens proportionally across all wallets.
- *
- *      uFragment balances are internally represented with a hidden denomination, 'gons'.
- *      We support splitting the currency in expansion and combining the currency on contraction by
- *      changing the exchange rate between the hidden 'gons' and the public 'fragments'.
- */
 contract HelpCoin is ERC20Detailed, Ownable {
-    // PLEASE READ BEFORE CHANGING ANY ACCOUNTING OR MATH
-    // Anytime there is division, there is a risk of numerical instability from rounding errors. In
-    // order to minimize this risk, we adhere to the following guidelines:
-    // 1) The conversion rate adopted is the number of gons that equals 1 fragment.
-    //    The inverse rate must not be used--TOTAL_GONS is always the numerator and _totalSupply is
-    //    always the denominator. (i.e. If you want to convert gons to fragments instead of
-    //    multiplying by the inverse rate, you should divide by the normal rate)
-    // 2) Gon balances converted into Fragments are always rounded down (truncated).
-    //
-    // We make the following guarantees:
-    // - If address 'A' transfers x Fragments to address 'B'. A's resulting external balance will
-    //   be decreased by precisely x Fragments, and B's external balance will be precisely
-    //   increased by x Fragments.
-    //
-    // We do not guarantee that the sum of all balances equals the result of calling totalSupply().
-    // This is because, for any conversion function 'f()' that has non-zero rounding error,
-    // f(x0) + f(x1) + ... + f(xn) is not always equal to f(x0 + x1 + ... xn).
+
     using SafeMath for uint256;
     using SafeMathInt for int256;
 
@@ -91,19 +65,13 @@ contract HelpCoin is ERC20Detailed, Ownable {
     // EIP-2612: keeps track of number of permits per address
     mapping(address => uint256) private _nonces;
 
-    /**
-     * @param monetaryPolicy_ The address of the monetary policy contract to use for authentication.
-     */
+
     function setMonetaryPolicy(address monetaryPolicy_) external onlyOwner {
         monetaryPolicy = monetaryPolicy_;
         emit LogMonetaryPolicyUpdated(monetaryPolicy_);
     }
 
-    /**
-     * @dev Notifies Fragments contract about a new rebase cycle.
-     * @param supplyDelta The number of new fragment tokens to add into circulation via expansion.
-     * @return The total number of fragments after the supply adjustment.
-     */
+ 
     function rebase(uint256 epoch, int256 supplyDelta)
         external
         onlyMonetaryPolicy
@@ -126,16 +94,7 @@ contract HelpCoin is ERC20Detailed, Ownable {
 
         _gonsPerFragment = TOTAL_GONS.div(_totalSupply);
 
-        // From this point forward, _gonsPerFragment is taken as the source of truth.
-        // We recalculate a new _totalSupply to be in agreement with the _gonsPerFragment
-        // conversion rate.
-        // This means our applied supplyDelta can deviate from the requested supplyDelta,
-        // but this deviation is guaranteed to be < (_totalSupply^2)/(TOTAL_GONS - _totalSupply).
-        //
-        // In the case of _totalSupply <= MAX_UINT128 (our current supply cap), this
-        // deviation is guaranteed to be < 1, so we can omit this step. If the supply cap is
-        // ever increased, it must be re-included.
-        // _totalSupply = TOTAL_GONS.div(_gonsPerFragment)
+
 
         emit LogRebase(epoch, _totalSupply);
         return _totalSupply;
@@ -155,48 +114,31 @@ contract HelpCoin is ERC20Detailed, Ownable {
         emit Transfer(address(0x0), owner_, _totalSupply);
     }
 
-    /**
-     * @return The total number of fragments.
-     */
     function totalSupply() external view override returns (uint256) {
         return _totalSupply;
     }
 
-    /**
-     * @param who The address to query.
-     * @return The balance of the specified address.
-     */
+   
     function balanceOf(address who) external view override returns (uint256) {
         return _gonBalances[who].div(_gonsPerFragment);
     }
 
-    /**
-     * @param who The address to query.
-     * @return The gon balance of the specified address.
-     */
+ 
     function scaledBalanceOf(address who) external view returns (uint256) {
         return _gonBalances[who];
     }
 
-    /**
-     * @return the total number of gons.
-     */
+    
     function scaledTotalSupply() external pure returns (uint256) {
         return TOTAL_GONS;
     }
 
-    /**
-     * @return The number of successful permits by the specified address.
-     */
+  
     function nonces(address who) public view returns (uint256) {
         return _nonces[who];
     }
 
-    /**
-     * @return The computed DOMAIN_SEPARATOR to be used off-chain services
-     *         which implement EIP-712.
-     *         https://eips.ethereum.org/EIPS/eip-2612
-     */
+
     function DOMAIN_SEPARATOR() public view returns (bytes32) {
         uint256 chainId;
         assembly {
@@ -214,12 +156,7 @@ contract HelpCoin is ERC20Detailed, Ownable {
             );
     }
 
-    /**
-     * @dev Transfer tokens to a specified address.
-     * @param to The address to transfer to.
-     * @param value The amount to be transferred.
-     * @return True on success, false otherwise.
-     */
+ 
     function transfer(address to, uint256 value)
         external
         override
@@ -251,12 +188,7 @@ contract HelpCoin is ERC20Detailed, Ownable {
         return true;
     }
 
-    /**
-     * @dev Function to check the amount of tokens that an owner has allowed to a spender.
-     * @param owner_ The address which owns the funds.
-     * @param spender The address which will spend the funds.
-     * @return The number of tokens still available for the spender.
-     */
+ 
     function allowance(address owner_, address spender) external view override returns (uint256) {
         return _allowedFragments[owner_][spender];
     }
@@ -300,17 +232,6 @@ contract HelpCoin is ERC20Detailed, Ownable {
         return true;
     }
 
-    /**
-     * @dev Approve the passed address to spend the specified amount of tokens on behalf of
-     * msg.sender. This method is included for ERC20 compatibility.
-     * increaseAllowance and decreaseAllowance should be used instead.
-     * Changing an allowance with this method brings the risk that someone may transfer both
-     * the old and the new allowance - if they are both greater than zero - if a transfer
-     * transaction is mined before the later approve() call is mined.
-     *
-     * @param spender The address which will spend the funds.
-     * @param value The amount of tokens to be spent.
-     */
     function approve(address spender, uint256 value) external override returns (bool) {
         _allowedFragments[msg.sender][spender] = value;
 
@@ -318,13 +239,7 @@ contract HelpCoin is ERC20Detailed, Ownable {
         return true;
     }
 
-    /**
-     * @dev Increase the amount of tokens that an owner has allowed to a spender.
-     * This method should be used instead of approve() to avoid the double approval vulnerability
-     * described above.
-     * @param spender The address which will spend the funds.
-     * @param addedValue The amount of tokens to increase the allowance by.
-     */
+
     function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
         _allowedFragments[msg.sender][spender] = _allowedFragments[msg.sender][spender].add(
             addedValue
@@ -334,12 +249,7 @@ contract HelpCoin is ERC20Detailed, Ownable {
         return true;
     }
 
-    /**
-     * @dev Decrease the amount of tokens that an owner has allowed to a spender.
-     *
-     * @param spender The address which will spend the funds.
-     * @param subtractedValue The amount of tokens to decrease the allowance by.
-     */
+
     function decreaseAllowance(address spender, uint256 subtractedValue) external returns (bool) {
         uint256 oldValue = _allowedFragments[msg.sender][spender];
         _allowedFragments[msg.sender][spender] = (subtractedValue >= oldValue)
@@ -350,16 +260,6 @@ contract HelpCoin is ERC20Detailed, Ownable {
         return true;
     }
 
-    /**
-     * @dev Allows for approvals to be made via secp256k1 signatures.
-     * @param owner The owner of the funds
-     * @param spender The spender
-     * @param value The amount
-     * @param deadline The deadline timestamp, type(uint256).max for max deadline
-     * @param v Signature param
-     * @param s Signature param
-     * @param r Signature param
-     */
     function permit(
         address owner,
         address spender,
